2306437b33f37a85610398ce7cfc3d4ee1341119
Silvan Groenveld <silvan.groenveld@gmail.com>
2024-04-19 13:29:11 +0200

    you can now remove items from your cart and if you press the buy now button it gets added to the library table

--- cart.php
+++ cart.php
@@ -31,6 +31,19 @@ IF ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header('Location: cart.php');
 }
 
+IF ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $sql = 'INSERT INTO library (UserID, GameID) VALUES (?, ?)';
+    $stmt = $pdo->prepare($sql);
+    foreach ($cart as $item) {
+        $stmt->execute([$_SESSION['loggedInUserId'], $item['GameID']]);
+    }
+
+    $sql = 'DELETE FROM winkelwagen WHERE UserID = ?';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_SESSION['loggedInUserId']]);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -59,30 +72,38 @@ IF ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <main>
     <div class="mx-auto px-8 py-8 max-w-3xl backdrop-blur border-solid border border-gray-300 rounded-t-lg shadow-2xl cart-center">
         <?php
-        foreach ($games as $game) {
-            ?>
-            <div class="cart-item flex justify-between">
-                <div class="flex items-center">
-                <img src="./images/<?= $game['Foto'] ?>">
-                <div class="item-details">
-                    <h2><?= $game['Title'] ?></h2>
-                    <p>Price: € <?= $game['Price'] ?></p>
+        $prijs = 0;
+        if (empty($games)) { ?>
+            <h1 style="font-size: 30px; color: lightgray;">Nothing to see here...</h1>
+        <?php
+        } else {
+            foreach ($games as $game) {
+    
+                $prijs += $game['Price'];
+                ?>
+                <div class="cart-item flex justify-between">
+                    <div class="flex items-center">
+                    <img src="./images/<?= $game['Foto'] ?>">
+                    <div class="item-details">
+                        <h2><?= $game['Title'] ?></h2>
+                        <p>Price: € <?= $game['Price'] ?></p>
+                    </div>
+                    </div>
+                    <form method="POST" style="display: inline-block;">
+                        <input type="hidden" name="game" value="<?= $game['GameID'] ?>">
+                        <button type="submit" class="logout">remove</button>
+                    </form>
                 </div>
-                </div>
-                <form method="POST" style="display: inline-block;">
-                    <input type="hidden" name="game" value="<?= $game['GameID'] ?>">
-                    <button type="submit" class="logout">remove</button>
-                </form>
-            </div>
             <?php
+            }
         }
         ?>
     </div>
 
-        <div class="holder mx-auto px-8 py-8 max-w-3xl rounded-b-lg">
-            <h2>Estimated Total: $19.99</h2>
+        <div class="holder mx-auto px-8  max-w-3xl rounded-b-lg flex justify-between items-center" style="color: lightgray;">
+            <h2>Estimated Total: €<?= $prijs ?></h2>
             <form method="POST" style="display: flex; float: right;">
-                    <button type="submit" class="btn btn-success">a</button>
+                    <button type="submit" class="btn btn-success">Buy Now!</button>
                 </form>
         </div>
     </main>

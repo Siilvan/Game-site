f2272110ac8fcb4c85618c9c8a452665ef0a07a0
Silvan Groenveld <silvan.groenveld@gmail.com>
2024-04-25 11:22:33 +0200

    can now filter based on genre at the store page

--- indexa.php
+++ indexa.php
@@ -28,6 +28,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt->execute();
     $games = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+if (isset($_GET['genre'])) {
+    $sql = 'SELECT * FROM games WHERE Genre = :genre';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam('genre', $_GET['genre']);
+    $stmt->execute();
+    $games = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 </head>
 <body>
@@ -49,6 +57,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <input type="text" placeholder="Search.." name="search" id="search">
             <button type="submit"><img src="images/search.jpg"></button>
         </form>
+        <div class="dropdown">
+            <button class="dropbtn"><img src="images/filter.png"></button>
+            <div class="dropdown-content">
+                <?php
+                $sql = 'SELECT DISTINCT Genre FROM games';
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute();
+                $genres = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($genres as $genre) {
+                    echo '<a href="indexa.php?genre=' . $genre['Genre'] . '" style="background-image: linear-gradient(to right, rgb(45, 61, 74), rgb(87, 101, 116)); color: white;">' . $genre['Genre'] . '</a>';
+                }
+                ?>
+            </div>
+        </div>
+
     </div>
 </div>
 <div class="mx-auto my-auto px-8 py-8 max-w-5xl backdrop-blur border-solid border border-gray-300 rounded-b-lg shadow-2xl width">

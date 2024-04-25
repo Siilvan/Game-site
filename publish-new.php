f7d73a511e2ad51a620171d5ba99866bcd02ab28
Silvan Groenveld <silvan.groenveld@gmail.com>
2024-04-25 11:51:04 +0200

    k

--- publish-new.php
+++ publish-new.php
@@ -31,6 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["image"]) && isset($_F
         $stmt->execute();
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -82,7 +83,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["image"]) && isset($_F
     <input type="file" name="image" id="image" class="hidden">
     <label for="image" class="register">Select your cover art</label>
 
-    <input type="file" name="banner" id="banner" class="hidden">
+    <input type="file" name="banner" id="banner" class="hidden">Select your banner</label>">
     <label for="banner" class="register">Select your banner</label>
     <br>
 
